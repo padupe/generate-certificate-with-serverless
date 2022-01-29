a589435f9ba97300fce07b7e6e4b6cb710983556
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-01-29 16:20:39 -0300

    feat(dynamodbClient.ts): new file added to the Project. Dir: src/utils.

---
+++ src/utils/dynamodbClient.ts
@@ -0,0 +1,16 @@
+import { DynamoDB } from "aws-sdk";
+
+const options = {
+    region: "localhost",
+    // 8000 é a porta padrão do DynamoDB
+    endpoint: "http://localhost:8000",
+}
+
+const isOffline = () => {
+    // Ao instalar o plugin "serverless-offline", a variável de ambiente IS_OFFLINE será setada como TRUE de maneira dinâmica
+    // Assim sendo, não há necessidade de se criar um ".env"
+    return process.env.IS_OFFLINE
+}
+
+// Se o retorno de "isOffline" for TRUE, o DynamoDB deve se conectar localmente, caso contrário na AWS
+export const document = isOffline() ? new DynamoDB.DocumentClient(options) : new DynamoDB.DocumentClient();
